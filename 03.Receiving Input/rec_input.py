32fe67dce7066e3ea8c24599a0dfbbd1829f3b66
swapnilcchougule <swapnilcchougule@gmail.com>
2022-10-14 13:16:51 +0200

    Reeciving input

---
+++ 03.Receiving Input/rec_input.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3    
+
+name = input('Type your name : ')             # The input() function always returns data as a string.
+birth_year = int(input('Type Birth_year: '))  # converting the result into an integer by calling the built-in int() function. 
+print('typed name and age are : ', name,birth_year)
