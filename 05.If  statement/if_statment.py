3b5d551cc35f384b2855eaeb06df924d04a12d2b
swapnilcchougule <swapnilcchougule@gmail.com>
2022-10-14 16:08:09 +0200

    if statement

---
+++ 05.If  statement/if_statment.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" if statement """
+
+var = int (input ("input the value :" ))
+if (var == 100): 
+	print ("Value is 100")
+elif (var == 50): 
+	print ("Value is 50")
+else:
+	print ("Value is",var)
+		
+print ("Good bye!")
+
+
+
