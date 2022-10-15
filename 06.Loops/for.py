a88495e004dc60ef5f7f7027821c9fa1357a4ddf
swapnilcchougule <swapnilcchougule@gmail.com>
2022-10-15 13:05:24 +0200

    for loop

---
+++ 06.Loops/for.py
@@ -0,0 +1,57 @@
+#!/use/bin/python3
+
+for i in range(1,5):    #to iterate between 1 to 5  :- 1,2,3,4
+	print(i)
+	
+print('*' * 20 )	
+	
+for count in range(5):  #to iterate between 0 to 5 :- 0,1,2,3,4
+	print(count)
+
+print('*' * 20 )
+
+for count in range(1,5,2):  #to iterate between 1 to 5 :- 1,_,3,_,
+	print(count)
+
+print('*' * 20 )
+
+for count in range(1,10,2):  #to iterate between 0 to 5 :- 1,_,3,_,5,_,7,_,9
+	print(count)
+	
+print('*' * 20 )
+
+for letter in 'Swapnil':
+	print("Current letter is : ", letter)	
+	
+print('*' * 20 )
+
+fruits = ['banana', 'apple',  'mango']
+for fruit in fruits:
+	print("current fruit : ", fruit) 
+
+print('*' * 20 )	
+	
+for index in range(len(fruits)):
+	   print ('Current fruit :', fruits[index])
+
+print('*' * 20 )
+count = range(5)
+print(count)
+print('*' * 20 )
+count = range(1,5)
+print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
