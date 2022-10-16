d73e6179290e197cfa1137e59c1d6b3786ca9537
swapnilcchougule <swapnilcchougule@gmail.com>
2022-10-16 12:17:40 +0200

    function

---
+++ 08.function/function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Function definition is here
+def printme( str ):
+   "This prints a passed string into this function"
+   print (str)
+def square(number):
+	return number * number     
+
+# function call
+printme("First call..!")
+printme("Second call..!")
+result = square(5)
+print(result) 
