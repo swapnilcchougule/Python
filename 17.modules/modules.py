04fa69c69d0cca39929672c00dbee169b4ecde16
swapnilcchougule <swapnilcchougule@gmail.com>
2022-10-17 23:04:06 +0200

    modules

---
+++ 17.modules/modules.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# A module is a file with some Python code. 
+# We use modules to break up our program into multiple files. 
+# There are 2 ways to import modules: we can 
+# 1. import the entire module 
+# 2. import specific objects in a module.
+
+import converters                         # importing the entire converters module
+print(converters.kg_to_lbs(70))
+
+from converters import lbs_to_kg          # importing one function in the converters module
+print(lbs_to_kg(155.55555555555554))
+
+
