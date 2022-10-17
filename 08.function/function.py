f5c939d243c4b6568cb9dbfb89ea1f01c2852573
swapnilcchougule <swapnilcchougule@gmail.com>
2022-10-17 10:27:14 +0200

    function

--- 08.function/function.py
+++ 08.function/function.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 
 # Function definition is here
-def printme( str ):
+
+def printme(str):
    "This prints a passed string into this function"
    print (str)
+
+
+def greet(name):
+   print (f'Hi welcome {name} ..!')   # formated string
+   
+   
 def square(number):
-	return number * number     
+	return number * number   
+	  
 
 # function call
 printme("First call..!")
 printme("Second call..!")
+greet( "john" )            # pass argument "john" to parameter "name"
+greet( "Swapnil" )
 result = square(5)
 print(result) 
